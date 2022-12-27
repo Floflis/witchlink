89df9bc0c8ec2e80857454bca96fea3de451e820
Daniella Mesquita <mesquitadaniella@proton.me>
2022-12-26 21:15:07 -0300

    cleanse effect

--- combine.sh
+++ combine.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-[ -p foobar.wl ] && rm foobar.wl
+file_name="file3"
+file_ext="txt"
 
-trap "[ -p foobar.wl ] && rm foobar.wl && exit" SIGINT
+[ -p file3.txt.wl ] && rm file3.txt.wl
+
+trap "[ -p $file_name.$file_ext.wl ] && rm $file_name.$file_ext.wl && exit" SIGINT
 
 while [  true ]; do
-  mkfifo foobar.wl
-  echo "Writing File"
-  cat file1.txt file2.txt > foobar.wl
-  rm foobar.wl
+  mkfifo $file_name.$file_ext.wl
+  echo "Served $file_name.$file_ext witchlink."
+  echo $(git cat-file -p 9eb4c0c897d21ac7db39a5d1a25bbeacf51c3ad4)$'\n'$(git cat-file -p ed1f67e477de14798295ed6091452354196e261d) > $file_name.$file_ext.wl
+#git object cat insp from https://stackoverflow.com/a/56870314/5623661
+#echo newline insp from https://stackoverflow.com/a/8467448/5623661
+  rm $file_name.$file_ext.wl
 done
 #insp from https://superuser.com/a/763200/1619518 <3
+
+#task: comment in https://superuser.com/a/763200/1619518 about mkfifo getting from files only at read, and not contantly on RAM
