19275401546ee1974893667fc1a1de58c78ed83e
Daniella Mesquita <mesquitadaniella@proton.me>
2023-02-02 14:15:53 -0300

    fix previous commit

--- install-notready.sh
+++ install-notready.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-flouser=$(logname)
-
-is_root=false
-if [ "$([[ $UID -eq 0 ]] || echo "Not root")" = "Not root" ]
-   then
-      is_root=false
-   else
-      is_root=true
-fi
-maysudo=""
-if [ "$is_root" = "false" ]
-   then
-      maysudo="sudo"
-   else
-      maysudo=""
-fi
-
 echo "Installing mimetypes and their icons..." # this is continuously adding the same entries to /etc/mime.types and have to be fixed
 cat >> /etc/mime.types <<EOF
 application/x-wl			        wl
@@ -34,8 +16,8 @@ cat > /usr/share/mime/packages/x-wl.xml <<EOF
 </mime-info>
 
 EOF
-$maysudo update-mime-database /usr/share/mime
+sudo update-mime-database /usr/share/mime
 
 echo "Installing icons for .wl files..."
 cp include/application-x-wl.svg /usr/share/icons/hicolor/scalable/mimetypes/
-$maysudo gtk-update-icon-cache /usr/share/icons/gnome/ -f
+sudo gtk-update-icon-cache /usr/share/icons/gnome/ -f
